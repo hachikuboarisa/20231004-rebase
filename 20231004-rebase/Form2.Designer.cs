86873d4bd9e98b6e48718cc71c6adfc0194340a3
有彩 八久保 <HachikuboArisa@anciel.co.jp>
2023-10-04 15:54:48 +0900

    TextBOX追加

--- 20231004-rebase/Form2.Designer.cs
+++ 20231004-rebase/Form2.Designer.cs
@@ -23,12 +23,43 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(52, 90);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(128, 245);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 19);
+			this.textBox1.TabIndex = 1;
+			// 
+			// Form2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form2";
 			this.Text = "Form2";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
