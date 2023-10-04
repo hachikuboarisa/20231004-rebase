5795badccb9d974a064d381a0ea4aec089616465
有彩 八久保 <HachikuboArisa@anciel.co.jp>
2023-10-04 15:55:05 +0900

    Button追加

--- 20231004-rebase/Form2.Designer.cs
+++ 20231004-rebase/Form2.Designer.cs
@@ -25,6 +25,7 @@
 		private void InitializeComponent() {
 			this.label1 = new System.Windows.Forms.Label();
 			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -43,11 +44,21 @@
 			this.textBox1.Size = new System.Drawing.Size(100, 19);
 			this.textBox1.TabIndex = 1;
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(265, 90);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
 			// Form2
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.label1);
 			this.Name = "Form2";
@@ -61,5 +72,6 @@
 
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
