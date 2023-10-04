fbf47b912bd1b33884544a7f21898000b441f53e
有彩 八久保 <HachikuboArisa@anciel.co.jp>
2023-10-04 15:52:17 +0900

    ListBox追加

--- 20231004-rebase/Form1.Designer.cs
+++ 20231004-rebase/Form1.Designer.cs
@@ -24,6 +24,7 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -33,11 +34,21 @@
 			this.textBox1.Size = new System.Drawing.Size(100, 19);
 			this.textBox1.TabIndex = 0;
 			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(64, 174);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(120, 88);
+			this.listBox1.TabIndex = 1;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listBox1);
 			this.Controls.Add(this.textBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -49,6 +60,7 @@
 		#endregion
 
 		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox1;
 	}
 }
 
